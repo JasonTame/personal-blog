7e3d9b8472c4d4ecd4d6725e3c6a471c78d57fd9
Jason Tame <jason@team.interaction-design.org>
2023-04-01 10:50:03 +0200

    Instal vitel PWA plugin

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/tailwind.css',
-                'resources/js/site.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ["resources/css/tailwind.css", "resources/js/site.js"],
+      refresh: true,
+    }),
+    VitePWA(),
+  ],
 });
